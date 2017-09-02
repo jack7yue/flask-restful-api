5baca79149fb2890ed008acc797a102bcc82fcb0
Jack <jack11m@hotmail.com>
2017-09-02 00:01:13 -0400

    connected to local mongo client

--- app.py
+++ app.py
@@ -1,47 +1,39 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-from flask_restful import Resource, Api
-
+from flask import Flask, jsonify
+from flask_pymongo import MongoClient
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
-
-app.config['MONGO_DBNAME'] = 'flask_learning'
-app.config['MONGO_URI'] = 'mongodb://user1:asd123@ds161823.mlab.com:61823/flask_learning'
-
 api = Api(app)
-db = PyMongo(app)
 
-players = [
-    {
-        'id': 1,
-        'name': 'Stephen Curry',
-        'position': 'PG'
-    },
-    {
-        'id': 2,
-        'name': 'Lebron James',
-        'position': 'SF/PF',
-    },
-    {
-        'id': 3,
-        'name': 'Kevin Durant',
-        'position': 'SF',
-    }
-    ]
-
+client = MongoClient('localhost', 27017)
+db = client['Players']
 
 class PlayerAPI(Resource):
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('id', type=int, location='json')
+        self.reqparse.add_argument('name', type=str, location='json')
+        self.reqparse.add_argument('is_active', type=bool, location='json')
+
+    def post(self):
+        args = self.reqparse.parse_args()
+        db.Players.insert(args)
+
     def get(self, id):
-        pass
+        cursor = db.Players.find_one({'id': id})
+        player = {'name': cursor['name'], 'position': cursor['position']}
+        return player
 
     def put(self, id):
-        pass
+        args = self.reqparse.parse_args()
+        db.Players.update({'id': id}, {'$set': args})
 
     def delete(self, id):
-        pass
+        db.Players.delete_many({'id': id})
 
-api.add_resource(PlayerAPI, '/players/<int:id>')
-
-
+api.add_resource(PlayerAPI, '/api/<int:id>')
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
--- db_access_layer.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-
-class DataAccessor():
-
-    def get
